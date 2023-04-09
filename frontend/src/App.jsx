c159b9be99fb4a13ff29aa56ebc2d25e04ea455f
Brian Kim <bhk3164@gmail.com>
2023-04-08 21:05:54 -0700

    Finished functionality to display all of the quotes within the timeframe.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,14 @@
 import "./App.css";
 import React, { useState } from "react";
+import Quotes from './Quotes.jsx';
 
 
 function App() {
 	const [name, setName] = useState("");
 	const [message, setMessage] = useState("");
 	const [time, setTime] = useState(1);
+	const [data, setData] = useState("");
+
 
 
 	const handleSubmit = event => {
@@ -18,7 +21,8 @@ function App() {
 			});
 
 			const quotes = await response.json()
-			console.log(quotes)
+			setData(quotes)
+
 		}
 
 		const postData = async () => {
@@ -31,9 +35,36 @@ function App() {
 		postData()
 		getData()
 
-
-
 	};
+	if (data) {
+		return (
+			<div className="App">
+				{/* TODO: include an icon for the quote book */}
+				<h1>Hack @ UCI Tech Deliverable</h1>
+
+				<h2>Submit a quote</h2>
+				{/* TODO: implement custom form submission logic to not refresh the page */}
+				<form onSubmit={handleSubmit}>
+					<label htmlFor="input-name" >Name</label>
+					<input onChange={(event) => setName(event.target.value)} type="text" name="name" id="input-name" required />
+					<label htmlFor="input-message">Quote</label>
+					<input onChange={(event) => setMessage(event.target.value)} type="text" name="message" id="input-message" required />
+					<select onChange={(event) => setTime(event.target.value)}>
+						<option value="1" selected>Last day</option>
+						<option value="7">Last week</option>
+						<option value="30">Last month</option>
+						<option value="365">Last year</option>
+					</select>
+					<button type="submit">Submit</button>
+				</form>
+
+				<h2>Previous Quotes</h2>
+				<div className="messages">
+					<Quotes {...data} />
+				</div>
+			</div>
+		);
+	}
 	return (
 		<div className="App">
 			{/* TODO: include an icon for the quote book */}
@@ -58,9 +89,7 @@ function App() {
 			<h2>Previous Quotes</h2>
 			{/* TODO: Display the actual quotes from the database */}
 			<div className="messages">
-				<p>Peter Anteater</p>
-				<p>Zot Zot Zot!</p>
-				<p>Every day</p>
+
 			</div>
 		</div>
 	);
