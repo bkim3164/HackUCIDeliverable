33c400ddaa026860562d977e944635f65d9c8f54
Brian Kim <bhk3164@gmail.com>
2023-04-09 13:30:18 -0700

    Worked on css and html adjustments for the form

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,8 @@
 import "./App.css";
 import React, { useState } from "react";
 import Quotes from './Quotes.jsx';
-
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 function App() {
 	const [name, setName] = useState("");
@@ -40,25 +41,28 @@ function App() {
 		return (
 			<div className="App">
 				{/* TODO: include an icon for the quote book */}
-				<h1>Hack @ UCI Tech Deliverable</h1>
+				<h1 >Hack @ UCI Tech Deliverable</h1>
 
-				<h2>Submit a quote</h2>
-				{/* TODO: implement custom form submission logic to not refresh the page */}
-				<form onSubmit={handleSubmit}>
-					<label htmlFor="input-name" >Name</label>
-					<input onChange={(event) => setName(event.target.value)} type="text" name="name" id="input-name" required />
-					<label htmlFor="input-message">Quote</label>
-					<input onChange={(event) => setMessage(event.target.value)} type="text" name="message" id="input-message" required />
-					<select onChange={(event) => setTime(event.target.value)}>
-						<option value="1" selected>Last day</option>
-						<option value="7">Last week</option>
-						<option value="30">Last month</option>
-						<option value="365">Last year</option>
-					</select>
-					<button type="submit">Submit</button>
-				</form>
+				<div className="submit-quote">
+					<h2>Submit a quote!</h2>
+					{/* TODO: implement custom form submission logic to not refresh the page */}
+					<form onSubmit={handleSubmit} className="form">
+						<label htmlFor="input-name" className="form-name">Name</label>
+						<input onChange={(event) => setName(event.target.value)} type="text" name="name" id="input-name" required />
+						<label htmlFor="input-message" className="form-quote">Quote</label>
+						<input onChange={(event) => setMessage(event.target.value)} type="text" name="message" id="input-message" required />
+						<label htmlFor="input-message" className="form-quote">Timeframe</label>
+						<select className="form-select" onChange={(event) => setTime(event.target.value)}>
+							<option value="1" selected>Last day</option>
+							<option value="7">Last week</option>
+							<option value="30">Last month</option>
+							<option value="365">Last year</option>
+						</select>
+						<button className="form-submit" type="submit">Submit</button>
+					</form>
+				</div>
 
-				<h2>Previous Quotes</h2>
+				<h2 className="quote" >Previous Quotes</h2>
 				<div className="messages">
 					<Quotes {...data} />
 				</div>
@@ -70,26 +74,23 @@ function App() {
 			{/* TODO: include an icon for the quote book */}
 			<h1>Hack @ UCI Tech Deliverable</h1>
 
-			<h2>Submit a quote</h2>
-			{/* TODO: implement custom form submission logic to not refresh the page */}
-			<form onSubmit={handleSubmit}>
-				<label htmlFor="input-name" >Name</label>
-				<input onChange={(event) => setName(event.target.value)} type="text" name="name" id="input-name" required />
-				<label htmlFor="input-message">Quote</label>
-				<input onChange={(event) => setMessage(event.target.value)} type="text" name="message" id="input-message" required />
-				<select onChange={(event) => setTime(event.target.value)}>
-					<option value="1" selected>Last day</option>
-					<option value="7">Last week</option>
-					<option value="30">Last month</option>
-					<option value="365">Last year</option>
-				</select>
-				<button type="submit">Submit</button>
-			</form>
-
-			<h2>Previous Quotes</h2>
-			{/* TODO: Display the actual quotes from the database */}
-			<div className="messages">
-
+			<div className="submit-quote">
+				<h2>Submit a quote!</h2>
+				{/* TODO: implement custom form submission logic to not refresh the page */}
+				<form onSubmit={handleSubmit} className="form">
+					<label htmlFor="input-name" className="form-name">Name</label>
+					<input onChange={(event) => setName(event.target.value)} type="text" name="name" id="input-name" required />
+					<label htmlFor="input-message" className="form-quote">Quote</label>
+					<input onChange={(event) => setMessage(event.target.value)} type="text" name="message" id="input-message" required />
+					<label htmlFor="input-message" className="form-quote">Timeframe</label>
+					<select className="form-select" onChange={(event) => setTime(event.target.value)}>
+						<option value="1" selected>Last day</option>
+						<option value="7">Last week</option>
+						<option value="30">Last month</option>
+						<option value="365">Last year</option>
+					</select>
+					<button className="form-submit" type="submit">Submit</button>
+				</form>
 			</div>
 		</div>
 	);
